e84746949bc93c839c8f8da70e1d22134714f4fa
mh-playground <hadjar.mark@icloud.com>
2026-02-05 22:47:14 -0500

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,8 +3,21 @@
 namespace Playground\Ci\Tests\ExampleTest;
 
 use PHPUnit\Framework\TestCase;
+use Playground\Ci\Example\Example;
 
+/**
+ * @covers \Playground\Ci\Example\Example
+ */
 class ExampleTest extends TestCase
 {
-    
+    public function testHelloWorldOutputsText(): void
+    {
+        $example = new Example();
+
+        ob_start();
+        $example->helloWorld();
+        $output = ob_get_clean();
+
+        self::assertSame('Hello World!', $output);
+    }
 }
